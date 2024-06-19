6bea89f6000753a35b59aec30c9ad4e9895cfd58
ChanDara Leav <chandara.leav@wbfinance.com.kh>
2024-06-19 08:29:52 +0700

    fix: fix run

---
+++ lib/app/main_staging.dart
@@ -0,0 +1,7 @@
+import 'package:check_list_app/config/flavors.dart';
+import 'package:check_list_app/main.dart' as runner;
+
+Future<void> main() async {
+  F.appFlavor = Flavor.staging;
+  await runner.main();
+}
